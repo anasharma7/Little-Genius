5df5164acff1ea6f71208b86dd4802ec366747e3
anasharma7 <117554760+anasharma7@users.noreply.github.com>
2025-10-26 23:06:08 -0400

    feat: integrate Footer component

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Projects />
         <Contact />
       </main>
+      <Footer />
     </>
   )
 }
